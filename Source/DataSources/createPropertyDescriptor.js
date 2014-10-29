eeff30303445944e4de15cc1ac25f67b967744da
Matthew Amato <matt.amato@gmail.com>
2014-10-28 20:13:26 -0400

    Minor cleanup.

--- Source/DataSources/createPropertyDescriptor.js
+++ Source/DataSources/createPropertyDescriptor.js
@@ -6,46 +6,38 @@ define([
         defaultValue,
         defined) {
     "use strict";
-	function createGetter(name, privateName, subscriptionName){
-		return function() {
-					console.log('get:' + privateName);
-					console.log('get:' + subscriptionName);
-	                return this[privateName];
-	            };
-			}
-			
-			function createSetter(name, privateName, subscriptionName){
-				return function(value) {
-				console.log('set:' + privateName);
-				console.log('set:' + subscriptionName);
-                var oldValue = this[privateName];
-                var subscription = this[subscriptionName];
-                if (defined(subscription)) {
-                    subscription();
-                    this[subscriptionName] = undefined;
-                }
-                if (oldValue !== value) {
-                    this[privateName] = value;
-                    this._definitionChanged.raiseEvent(this, name, value, oldValue);
-                }
-                if (defined(value) && defined(value.definitionChanged)) {
-                    this[subscriptionName] = value.definitionChanged.addEventListener(function() {
-                        this._definitionChanged.raiseEvent(this, name, value, value);
-                    }, this);
-                }
-            };
-			}
-				
-    function createProperty(name, privateName1, subscriptionName1, configurable) {
-		var privateName = privateName1;
-		var subscriptionName = subscriptionName1;
-		console.log('Dec:' + privateName);
-		console.log('Dec:' + subscriptionName);
-       
-	    return {
+
+    function createGetter(name, privateName, subscriptionName) {
+        return function() {
+            return this[privateName];
+        };
+    }
+
+    function createSetter(name, privateName, subscriptionName) {
+        return function(value) {
+            var oldValue = this[privateName];
+            var subscription = this[subscriptionName];
+            if (defined(subscription)) {
+                subscription();
+                this[subscriptionName] = undefined;
+            }
+            if (oldValue !== value) {
+                this[privateName] = value;
+                this._definitionChanged.raiseEvent(this, name, value, oldValue);
+            }
+            if (defined(value) && defined(value.definitionChanged)) {
+                this[subscriptionName] = value.definitionChanged.addEventListener(function() {
+                    this._definitionChanged.raiseEvent(this, name, value, value);
+                }, this);
+            }
+        };
+    }
+
+    function createProperty(name, privateName, subscriptionName, configurable) {
+        return {
             configurable : configurable,
-            get : createGetter(name, privateName1, subscriptionName1),
-            set : createSetter(name, privateName1, subscriptionName1)
+            get : createGetter(name, privateName, subscriptionName),
+            set : createSetter(name, privateName, subscriptionName)
         };
     }
 
